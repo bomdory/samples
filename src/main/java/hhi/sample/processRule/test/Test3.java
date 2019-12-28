9f9d897043702c62e36c64e616f0b3a0e4646a73
bomdory <cschae@hanmail.net>
2019-12-29 00:01:17 +0900

    Update Test3.java

--- src/main/java/hhi/sample/processRule/test/Test3.java
+++ src/main/java/hhi/sample/processRule/test/Test3.java
@@ -11,6 +11,7 @@ public class Test3 implements ProcessRule{
 	@Override
 	public Object proceess(ProcessOption option) throws Exception {
 //		나나나
+// 		
 		try {
 			Thread.sleep(3000);
 		} catch (InterruptedException e) {
