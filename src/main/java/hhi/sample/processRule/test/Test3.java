12340c3f5ed38a84f76f9838680e874bc1cd8948
cschae <cschae@hanmail.net>
2019-12-28 23:59:50 +0900

    .

--- src/main/java/hhi/sample/processRule/test/Test3.java
+++ src/main/java/hhi/sample/processRule/test/Test3.java
@@ -10,7 +10,7 @@ public class Test3 implements ProcessRule{
 
 	@Override
 	public Object proceess(ProcessOption option) throws Exception {
-//		ㅁㅁㅁ
+//		나나나
 		try {
 			Thread.sleep(3000);
 		} catch (InterruptedException e) {
