b725e22ddf6be1cad3ca7768514cacf7130e36bc
cscha <cscha@DESKTOP-4VJFP9N>
2019-12-28 22:51:31 +0900

    해결

--- src/main/java/hhi/sample/processRule/test/Test3.java
+++ src/main/java/hhi/sample/processRule/test/Test3.java
@@ -12,8 +12,7 @@ public class Test3 implements ProcessRule{
 	public Object proceess(ProcessOption option) throws Exception {
 		
 		//이거수정1
-		
-		
+//		난 이거 수정
 		try {
 			Thread.sleep(3000);
 		} catch (InterruptedException e) {
