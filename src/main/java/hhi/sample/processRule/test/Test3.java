f2b69851152780dda9fa4a1106ef217434b5ce3f
cscha <cscha@DESKTOP-4VJFP9N>
2019-12-28 22:53:12 +0900

    좀

--- src/main/java/hhi/sample/processRule/test/Test3.java
+++ src/main/java/hhi/sample/processRule/test/Test3.java
@@ -11,8 +11,7 @@ public class Test3 implements ProcessRule{
 	@Override
 	public Object proceess(ProcessOption option) throws Exception {
 		
-		//이거수정1
-//		난 이거 수정
+
 		try {
 			Thread.sleep(3000);
 		} catch (InterruptedException e) {
