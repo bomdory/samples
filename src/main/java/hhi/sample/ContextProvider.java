09afaf4886bfdcdd6234a5e3205ae7b74c4bbbd9
cschae <cschae@hanmail.net>
2019-12-29 22:25:45 +0900

    .

--- src/main/java/hhi/sample/ContextProvider.java
+++ src/main/java/hhi/sample/ContextProvider.java
@@ -13,23 +13,23 @@ public class ContextProvider implements ApplicationContextAware{
 //	ApplicationContextProvider appContext = ApplicationContextProvider.getApplicationContext()
 	private static ApplicationContext springContext;
 	@Override
-	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-		
-		this.springContext = applicationContext;
+	public void setApplicationContext(final ApplicationContext applicationContext) throws BeansException {
+
+		ContextProvider.springContext = applicationContext;
 	}
 
 	public static ApplicationContext getApplicationContext() {
 		return springContext;
 	}
-	
-	public static <T> T getBean(Class<T> klass) {
+
+	public static <T> T getBean( Class<T> klass) {
 		return springContext.getBean(klass);
 	}
-	
-	public static Object getBean(String beanName) {
+
+	public static Object getBean( String beanName) {
 		return springContext.getBean(beanName);
 	}
-	
+
 	public static ProcessRule getProcessBean(String beanName) {
 		return springContext.getBean(beanName, ProcessRule.class);
 	}
